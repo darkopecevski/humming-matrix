051ddce726cdea9610ac518e4811e41664b83168
Aneta <Aneta@Aneta-PC>
2012-11-01 22:53:35 +0100

    Initial commit

---
+++ src/hummingmatrix/v2/HummingMatrixV2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hummingmatrix.v2;
+
+/**
+ *
+ * @author Aneta
+ */
+public class HummingMatrixV2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
