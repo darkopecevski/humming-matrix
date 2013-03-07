15efddced331c1929ee81fed15d4c80215e28592
Aneta <Aneta@Aneta-PC>
2013-03-07 20:53:26 +0100

    Adding different slider

--- src/hummingmatrix/v2/HummingMatrixV2.java
+++ src/hummingmatrix/v2/HummingMatrixV2.java
@@ -4,6 +4,8 @@
  */
 package hummingmatrix.v2;
 
+import hummingmatrix.v2.panels.HomeFrame;
+
 /**
  *
  * @author Darko
@@ -14,6 +16,8 @@ public class HummingMatrixV2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        HomeFrame hf = new HomeFrame();
+        hf.setLocationRelativeTo(null);
+        hf.setVisible(true);
     }
 }
